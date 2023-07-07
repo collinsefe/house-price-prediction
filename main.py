d9bd089ea55a63ab9b6489e048d2c2ca8e281381
Collins Orighose <collinsefe@yahoo.com>
2023-07-07 13:49:44 +0100

    updated main

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ from ExtractLoad.utils.utils import ReadWriteFromS3, get_data
 def get_and_write_to_s3():
     df = get_data("https://raw.githubusercontent.com/Amberlynnyandow/dsc-1-final-project-online-ds-ft-021119/master/kc_house_data.csv")
 
-    writeToS3 = ReadWriteFromS3.create_con_string(bucket_name="thedevopsclassroom", key="dev/training")
+    writeToS3 = ReadWriteFromS3.create_con_string(bucket_name='bucketname', key='Key')
 
-    writeToS3.writeToS3(df=df, file_name="house_price")
+    writeToS3.writeToS3(df=df, file_name='filename')
 
 
 if __name__ == "__main__":
     get_and_write_to_s3()
+    filename = "house-price"
+    bucketname = "thedevopsclassroom"
+    Key = "dev"
+    
 
